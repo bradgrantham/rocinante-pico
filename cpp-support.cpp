264796a651eeaadba762b972841325c8c3715530
Brad Grantham <grantham@plunk.org>
2024-08-13 22:18:37 -0700

    fake filesystem for file chooser

--- cpp-support.cpp
+++ cpp-support.cpp
@@ -1,13 +1,18 @@
 #include <cstdio>
 #include <string>
 #include <vector>
+#include <map>
+#include <cstring>
 #include "byte_queue.h"
 #include "events.h"
 #include "events_internal.h"
+#include "rocinante.h"
 
+#if 0
 void LauncherRegisterApp(const std::string& name, const std::string& exe_name, const std::string& what_to_choose, const std::string& where_to_choose, const std::string& suffix, const std::vector<std::string>& first_args, const std::vector<std::string>& last_args, int (*main)(int argc, const char **argv))
 {
 }
+#endif
 
 void enqueue_press_release(int keycap)
 {
@@ -129,6 +134,53 @@ void CheckEvents(void)
                 break;
         }
     }
+}
+
+const std::map<std::string, std::vector<std::string>> filesystem =
+{
+    {"coleco", { "COLECO.ROM", "Smurf - Rescue in Gargamel's Castle (1982).col", "Zaxxon (1982) (Sega).col" } },
+};
+
+Status RoFillFilenameList(const char* dirName, uint32_t flags, const char* optionalFilterSuffix, size_t maxNames, char **filenames, size_t* filenamesSize)
+{
+    Status status;
+
+    const auto iter = filesystem.find(dirName);
+    if (iter != filesystem.end()) {
+
+        for (const auto& entry: iter->second) 
+        {
+            int addToList = 1;
+
+            if (entry[entry.size() - 1] == '/') {                    /* It is a directory */
+                // XXX Really should have a way to descend into directories.
+                addToList = 0;
+            } else if((entry[0] == '.') && (flags & CHOOSE_FILE_IGNORE_DOTFILES)) {
+                addToList = 0;
+            } else if(optionalFilterSuffix && (strcmp(optionalFilterSuffix, entry.c_str() + entry.size() - strlen(optionalFilterSuffix)) != 0)) {
+                addToList = 0;
+            }
+
+            if(addToList) {
+                if(*filenamesSize > maxNames - 1) {
+                    return RO_RESOURCE_EXHAUSTED;
+                }
+                filenames[(*filenamesSize)++] = strdup(entry.c_str());
+            }
+        }
+        status = RO_SUCCESS;
+
+    } else {
+
+        if(*filenamesSize > maxNames - 1) {
+            return RO_RESOURCE_EXHAUSTED;
+        }
+        filenames[(*filenamesSize)++] = strdup("failed to find dir");
+        status = RO_RESOURCE_NOT_FOUND;
+
+    }
+
+    return status;
 }
 
 };
